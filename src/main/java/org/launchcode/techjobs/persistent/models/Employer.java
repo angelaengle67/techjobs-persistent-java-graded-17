17edfd37b32435c3affbb892465c1fa5c0657fae
Angela Engle <angelaengle67@gmail.com>
2023-11-28 20:00:21 -0600

    Completed through Task 3 up until Updating HomeController

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,11 +1,20 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class Employer extends AbstractEntity {
+    @OneToMany
+    @JoinColumn(name="employer_id")
+    private List<Job> jobs = new ArrayList<>();
     @NotNull
     @Size(min = 2, max = 100, message = "Location must be between 2 and 100 characters.")
     private String location;
