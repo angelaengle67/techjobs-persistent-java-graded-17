12f782784b62976d0b7f86771f5bab8f9a928578
Angela Engle <angelaengle67@gmail.com>
2023-12-03 15:47:31 -0600

    Completed through Task 3

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -1,8 +1,11 @@
 package org.launchcode.techjobs.persistent.controllers;
 
 import jakarta.validation.Valid;
+import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
 import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.launchcode.techjobs.persistent.models.data.JobRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,6 +23,10 @@ import java.util.Optional;
 public class HomeController {
     @Autowired
     private EmployerRepository employerRepository;
+    @Autowired
+    private JobRepository jobRepository;
+    @Autowired
+    private SkillRepository skillRepository;
 
     @RequestMapping("/")
     public String index(Model model) {
@@ -33,7 +40,7 @@ public class HomeController {
     public String displayAddJobForm(Model model) {
 	    model.addAttribute("title", "Add Job");
         model.addAttribute(new Job());
-        model.addAttribute("employer", employerRepository.findAll());
+        model.addAttribute("employers", employerRepository.findAll());
         return "add";
     }
 
@@ -44,7 +51,14 @@ public class HomeController {
         if (errors.hasErrors()) {
 	    model.addAttribute("title", "Add Job");
             return "add";
-        }
+        } else {
+             //if employer already exists in database, set employerId for this new job, otherwise, create new employer object, which will create the ID for employer
+                Employer employer = employerRepository.findById(employerId).orElse(new Employer());
+                newJob.setEmployer(employer);
+                //create an Employer employer object
+                jobRepository.save(newJob); //new job instance off new job class-setEmployer(EmployerObject) then save
+            }
+        //array list of skills needs the list set, set inside of new job object, need skills object, then put skills object inside of new job object
 
         return "redirect:";
     }
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class Employer extends AbstractEntity {
     @OneToMany
     @JoinColumn(name="employer_id")
-    private List<Job> jobs = new ArrayList<>();
+    private final List<Job> jobs = new ArrayList<>();
     @NotNull
     @Size(min = 2, max = 100, message = "Location must be between 2 and 100 characters.")
     private String location;
@@ -26,5 +26,9 @@ public class Employer extends AbstractEntity {
 
     public void setLocation(String location) {
         this.location = location;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
     }
 }
